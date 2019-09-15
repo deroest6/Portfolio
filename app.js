1626392438c4d8beaca87b1d6c3af86c812c5a95
Jantsen DeRoest <deroest6@hotmail.com>
2019-09-15 12:11:21 -0600

    Styled a fe components within the Form

--- app.js
+++ app.js
@@ -39,6 +39,49 @@ db.once('open', function() {
 // Interactive Javascript
 //
 
+//
+// Contact Form Validator
+//
+// $(function() {
+//
+//
+//   $("#contact-form").validator();
+//
+//   // when the form is submitted
+//   $("#contact-form").on("submit", function(e) {
+//     // if the validator does not prevent form submit
+//     if (!e.isDefaultPrevented()) {
+//       var url = "contact.php";
+//
+//
+//       var messageAlert = "alert-success";
+//       var messageText =
+//         "Your message was sent, thank you. I will get back to you soon.";
+//
+//       // let's compose Bootstrap alert box HTML
+//       var alertBox =
+//         '<div class="alert ' +
+//         messageAlert +
+//         ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' +
+//         messageText +
+//         "</div>";
+//
+//       // If we have messageAlert and messageText
+//       if (messageAlert && messageText) {
+//         // inject the alert to .messages div in our form
+//         $("#contact-form").find(".messages").html(alertBox);
+//         // empty the form
+//         $("#contact-form")[0].reset();
+//       }
+//
+//       return false;
+//     }
+//   });
+// });
+
+
+
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
