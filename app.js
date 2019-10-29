e26c4b44472798a2d27e49980c443541b6e7a0a9
Jantsen DeRoest <deroest6@hotmail.com>
2019-10-29 11:24:38 -0600

    Added .gitignore file, added env keys and configured nodemailer via the video

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 const mongodb = require('mongodb');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
@@ -29,15 +30,33 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
 });
+
+
 //TODO
 
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.GOOGLE_EMAIL,
+          pass: process.env.GOOGLE_EMAIL_PASS
+        }
+    });
 
+    let mailOptions = {
+      from: 'jantsenderoest@gmail.com',
+      to: 'deroest6@gmail.com',
+      subject: 'Testing and testing',
+      text: 'IT works'
+    };
 
-
-
-
-
-
+    transporter.sendMail(mailOptions, function(err, data) {
+      if(err) {
+        console.log('Error Occured: ' + err);
+      } else {
+        console.log('Email Sent');
+      }
+    });
 
 
 //
